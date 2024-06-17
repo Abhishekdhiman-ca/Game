9a5be6d2d3ed04085c1041492f41a2fb78f98367
Abhishek Dhiman <Abhishek.Dhiman@triosstudent.com>
2024-06-17 00:07:55 -0400

    S

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,7 +37,7 @@ app.use('/user', userRoutes);
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'App.js'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
